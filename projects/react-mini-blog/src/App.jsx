19ec30871d27ddb35dd56e8d8ded10c18f722184
민영 <104131962+y0ungg@users.noreply.github.com>
2022-08-22 22:56:53 +0900

    혼자서 mini blog 만들기 (3, 4) (#2)
    * 혼자서 mini blog 만들기 (3)
    
    * 혼자서 mini blog 만들기 (4)

--- projects/react-mini-blog/src/App.jsx
+++ projects/react-mini-blog/src/App.jsx
@@ -1,43 +1,61 @@
 import styled from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import MainPage from "./components/page/MainPage";
 import PostViewPage from "./components/page/PostViewPage";
 import PostWritePage from "./components/page/PostWritePage";
 
-const Nav = styled.nav`
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  list-style: none;
-  flex-wrap: wrap;
+// const Nav = styled.nav`
+//   padding: 0;
+//   margin: 0;
+//   box-sizing: border-box;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   list-style: none;
+//   flex-wrap: wrap;
+// `
+
+const StyledLink = styled(Link)`
+    color: black;
+    text-decoration: none;
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+`;
+
+const Title = styled.h1`
+  font-family: sans-serif;
+  text-align: center;
+  border: 1px solid green;
+  padding: 40px 0px;
+  font-size: 4rem;
+  font-weight: 100;
+  font-style: italic;
 `
 
-// const StyledLink = styled(Link)`
-//     text-decoration: none;
-//     color: black;
-//     width: 20%;
-//     text-align: center;
-//     padding: 5%;
-//     font-size: 2rem;
-
-//     &:focus, &:hover, &:visited, &:link, &:active {
-//         text-decoration: none;
-//     }
-// `;
+const Footer = styled.footer`
+  text-align: right;
+  margin-top: 50px;
+  padding: 10px 10px 30px 0;
+  background: #c1edf8;
+`
 
 function App() {
   return (
     <div className="App">
-      <h2>미니 블로그</h2>
+      <header>
+        <StyledLink to="/">
+          <Title>Mini blog</Title>
+        </StyledLink>
+      </header>
       <Routes>
         <Route path='/write' element={<PostWritePage />} />
         <Route index element={<MainPage />} />
         <Route path='/post/:postId' element={<PostViewPage />} />
       </Routes>
+      <Footer>나의 미니 블로그</Footer>
     </div>
   );
 }
--- projects/react-mini-blog/src/components/page/PostWritePage.jsx
+++ projects/react-mini-blog/src/components/page/PostWritePage.jsx
@@ -1,13 +1,61 @@
-//글 (제목, 내용) 입력, 작성완료버튼
+import { useState } from "react";
+import data from "../../Data";
 
 const PostWritePage = () => {
-    return (
-      <>
-        <div>글 제목</div>
-        <div>내용</div>
-        <button>작성완료</button>
-      </>
-    );
+  const [title, setTitle] = useState("");
+  const [story, setStory] = useState("");
+  const [id, CountId] = useState(7);
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
   };
-  
-  export default PostWritePage;
+
+  const handleChangeStory = (event) => {
+    setStory(event.target.value);
+  };
+
+  const onSubmitInput = (event) => {
+    event.preventDefault();
+    CountId(id + 1);
+    const newPost = {
+      'id': id,
+      'title': title,
+      'content': story,
+      'comments': [
+        {'id': 1,
+        'content': '잘보고갑니다'
+      }]
+    }
+    //data에 새 글 추가
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmitInput}>
+        <div>
+          글제목
+          <input
+            onChange={handleChangeTitle}
+            id="title"
+            type="text"
+            name="title"
+            placeholder="제목을 입력해주세요"
+            value={title}
+          ></input>
+        </div>
+        <textarea
+          onChange={handleChangeStory}
+          name="story"
+          type="text"
+          placeholder="내용을 입력해주세요"
+          value={story}
+        ></textarea>
+        <section>
+          <input type="submit" value="작성완료"></input>
+        </section>
+      </form>
+    </>
+  );
+};
+
+export default PostWritePage;
