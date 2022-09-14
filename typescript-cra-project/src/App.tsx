698686d3e5b03d7b162334e81970fbe04091016c
y0ungg <m0_23@naver.com>
2022-09-14 23:29:58 +0900

    Create-React-App TypeScript

---
+++ typescript-cra-project/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+      <Todos items={['Learn React', 'Learn TypeScript']}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ typescript-cra-project/src/components/Todos.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Todos: React.FC<{items: string[]}> = (props) => {
+    return (
+    <ul>
+        {props.items.map(item => {
+            return (
+                <li key={item}>{item}</li>
+            )
+        })}
+    </ul>
+    )
+}
+
+export default Todos;
